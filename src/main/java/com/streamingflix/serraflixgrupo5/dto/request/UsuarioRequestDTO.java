b522ffb354b3cce19fb9192fe85180677a6ff251
Brun0Fr3itas <brunos8sfreitas@gmail.com>
2026-05-22 20:37:44 -0300

    feat: adição do código usuariorequestdto
    Adicionando código ao arquivo UsuarioRequestDTO

--- src/main/java/com/streamingflix/serraflixgrupo5/dto/request/UsuarioRequestDTO.java
+++ src/main/java/com/streamingflix/serraflixgrupo5/dto/request/UsuarioRequestDTO.java
@@ -1,5 +1,87 @@
 package com.streamingflix.serraflixgrupo5.dto.request;
 
-public class UsuarioRequestDTO {
+import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UsuarioRequestDTO {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "Nome é obrigatório")
+	@Size(min = 3, max = 100)
+	private String nome;
+	
+	@NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
+    @Column(unique = true, nullable = false)
+	private String email;
+	
+	@NotBlank(message = "Username é obrigatório")
+	@Size(min = 3, max = 50)
+	@Column(unique = true, nullable = false)
+	private String username;
+	
+	@NotBlank(message = "Senha é obrigatória")
+	@Size(min = 8, message = "Senha deve ter no mínimo 8 caracteres")
+	private String senha;
+	
+	@Column(nullable = false)
+	private LocalDate dataCriacao;
+
+	private Long getId() {
+		return id;
+	}
+
+	private void setId(Long id) {
+		this.id = id;
+	}
+
+	private String getNome() {
+		return nome;
+	}
+
+	private void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	private String getEmail() {
+		return email;
+	}
+
+	private void setEmail(String email) {
+		this.email = email;
+	}
+
+	private String getUsername() {
+		return username;
+	}
+
+	private void setUsername(String username) {
+		this.username = username;
+	}
+
+	private String getSenha() {
+		return senha;
+	}
+
+	private void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	private LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+
+	private void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
 }
