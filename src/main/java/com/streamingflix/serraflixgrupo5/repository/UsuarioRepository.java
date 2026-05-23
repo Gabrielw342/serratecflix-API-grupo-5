9c2fd5b43a755210b3ef3a4086bee1a26d0eb7ab
Brun0Fr3itas <brunos8sfreitas@gmail.com>
2026-05-22 21:28:47 -0300

    feat: adição do código usuario_repository
    Adicionando código ao arquivo UsuarioRepository

--- src/main/java/com/streamingflix/serraflixgrupo5/repository/UsuarioRepository.java
+++ src/main/java/com/streamingflix/serraflixgrupo5/repository/UsuarioRepository.java
@@ -1,5 +1,18 @@
 package com.streamingflix.serraflixgrupo5.repository;
 
-public interface UsuarioRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.streamingflix.serraflixgrupo5.entity.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	Optional<Usuario> findByUsername(String username);
+	Optional<Usuario> findByEmail(String email);
+	
+	boolean existsByUsername(String username);
+	boolean existsByEmail(String email);
 }
