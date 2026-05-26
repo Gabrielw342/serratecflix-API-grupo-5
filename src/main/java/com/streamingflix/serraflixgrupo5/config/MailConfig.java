67ec53b087df4ce75e216d20213e6455e5858b9b
EnzoBCCosta <enzocarneiro2006@gmail.com>
2026-05-26 00:37:51 -0300

    test: commit obrigatorio para mudar de branch

---
+++ src/main/java/com/streamingflix/serraflixgrupo5/config/MailConfig.java
@@ -0,0 +1,29 @@
+package com.streamingflix.serraflixgrupo5.config;
+
+import org.springframework.beans.factory.annotation.Autowired;  
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@Configuration
+public class MailConfig {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom("");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText("Cadastro realizado com sucesso! Bem-vindo ao Serraflix! \n" + text 
+                        + "\n\nAtenciosamente,\nEquipe Serraflix - Grupo 5");
+                        
+        mailSender.send(message);
+        
+    }
+
+
+}
