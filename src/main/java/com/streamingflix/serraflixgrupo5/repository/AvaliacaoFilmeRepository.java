0a756672a3c9682bb916bb151edc0728c96e5282
EnzoBCCosta <enzocarneiro2006@gmail.com>
2026-05-27 14:38:20 -0300

    fix: removendo merges errados da main

--- src/main/java/com/streamingflix/serraflixgrupo5/repository/AvaliacaoFilmeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.streamingflix.serraflixgrupo5.repository;
-
-import com.streamingflix.serraflixgrupo5.entity.AvaliacaoFilme;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AvaliacaoFilmeRepository extends JpaRepository<AvaliacaoFilme, Long> {
-}
